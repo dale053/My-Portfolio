11da51c5a259fe752ef1e11712140c7888dce19b
dale053 <rogers99311@outlook.com>
2025-08-07 13:07:22 -0700

    Modified UI

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
   title: "Portfolio of Dale Rogers - Software Developer",
   description:
     "This is the portfolio of Dale Rogers. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: '/profile_rounded.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
